c896766b385567b6a38edd08cb94162d814434b8
Kanji Furuhashi <pikanji@gmail.com>
2022-10-18 10:52:54 +0700

    feat: Support project custom php ini files in container control scripts

--- restart.sh
+++ restart.sh
@@ -42,6 +42,9 @@ docker exec -it $CONTAINER_NAME sh -c 'cd /etc/nginx/conf.d && find -L . -name .
 # Create symbolic link to nginx config files in .dev/nginx dir of each project dir.
 docker exec -it $CONTAINER_NAME sh -c 'find /var/www/html -maxdepth 4 -path "*/.dev/nginx/*.conf" | sed "s%\(/var/www/html/\)\([^/.]*\)\(/.*\)%\1\2\3 /etc/nginx/conf.d/\2.conf%" | xargs -n 2 ln -s 2> /dev/null'
 
+# Create symbolic link to nginx config files in .dev/php dir of each project dir.
+docker exec -it $CONTAINER_NAME sh -c 'find /var/www/html -maxdepth 4 -path "*/.dev/php/*.ini" | sed "s%\(/var/www/html/\)\([^/.]*\)\(/.*\)%\1\2\3 /etc/php.d/90-\2.ini%" | xargs -n 2 ln -s 2> /dev/null'
+
 # Add hostname set as server_name in nginx conf files in each application to /etc/hosts in the container.
 # This is required for some tests when phpunit is executed inside of the container.
 # The part from `2>&1 |...` is for excluding error message from directories that requires admin access.
